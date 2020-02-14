2f4a9d48e344f4ad95a4ce5e66a9fe45d08b73fa
robert hanzal <robert.hanzal@res-group.com>
2020-02-14 09:36:50 +0100

    updated the assert

--- tests/test_str2bool.py
+++ tests/test_str2bool.py
@@ -1,11 +1,10 @@
 import pytest
-from utilities.str2bool import str2bool
-
+from utilities.import str2bool
 def test_str2bool():
-    output_value = type(str2bool('y'))
-    expected_output_value = bool
+    output_value = str2bool('y')
+    expected_output_value = True
 
-    assert expected_value == actual_value
+    assert type(expected_output_value) == type(output_value)
 
 if __name__ == '__main__':
     test_str2bool()
