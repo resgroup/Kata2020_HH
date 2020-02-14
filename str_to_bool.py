b337b3ad1d3e273a8d00f473e5497964c3753b05
robert hanzal <robert.hanzal@res-group.com>
2020-02-14 10:04:43 +0100

    working version

--- str_to_bool.py
+++ str_to_bool.py
@@ -1,9 +1,13 @@
 def str2bool(v):
-  if isinstance(v, bool):
+    if isinstance(v, bool):
         return v
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
         return False
-   else:
+    else:
        return v
+
+
+if __name__ == '__main__':
+    print(str2bool('y'))
--- test_str2bool.py
+++ test_str2bool.py
@@ -1,9 +1,8 @@
 import pytest
-import str2bool
+from str_to_bool import str2bool
 def test_str2bool():
     output_value = str2bool('y')
     expected_output_value = True
-
     assert type(expected_output_value) == type(output_value)
 
 if __name__ == '__main__':
