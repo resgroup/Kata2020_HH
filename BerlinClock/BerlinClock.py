971b4d3596843a7c7c2169014cd291f8a01c45cc
robert hanzal <robert.hanzal@res-group.com>
2020-02-14 12:58:30 +0100

    Added the time to string functions

--- BerlinClock/BerlinClock.py
+++ BerlinClock/BerlinClock.py
@@ -1,2 +1,22 @@
 def integer_to_unery_string(digit_character, integer_value):
     return digit_character*integer_value
+
+def pad_unery_string(string, padded_length)
+    padding_length = padded_length - len(string)
+    if padding_length < 0:
+        Exception
+    return string + "0" * padding_length 
+
+def create_light_string(string):
+    string + "\n"
+
+def hours_to_string(hours):
+    return create_light_string(pad_unery_string(integer_to_unery_string(hours/5, 'R'), 4)) +\
+           create_light_string(pad_unery_string(integer_to_unery_string(hours%5, 'R'), 4))
+
+def minutes_to_string(minutes):
+    return create_light_string(pad_unery_string(integer_to_unery_string(minutes/5, 'Y'), 11)) +\
+           create_light_string(pad_unery_string(integer_to_unery_string(minutes%5, 'Y'), 4))
+
+def seconds_to_string(seconds):
+    return create_light_string(pad_unery_string(integer_to_unery_string(seconds%2, 'Y'), 1))
--- BerlinClock/BerlinClockTest.py
+++ BerlinClock/BerlinClockTest.py
@@ -8,3 +8,52 @@ def test_unery_yellow_11():
 def test_unery_red_7():
     result = integer_to_unery_string("R", 7)
     assert result == "RRRRRRR"    
+
+def test_pad_unery_string():
+    result = pad_unery_string('RRR', 7)
+    assert result == 'RRROOOO'
+
+def test_pad_unery_string_no_padding():
+    result = pad_unery_string('RRRR', 4)
+    assert result == 'RRRR'
+
+def test_pad_unery_string_emptry_string():
+    result = pad_unery_string('', 3)
+    assert result == '000'
+
+def test_pad_unery_string_too_long():
+    exception_thrown = false
+    try:
+        result = pad_unery_string('RRRR', 3)
+    except: 
+        exception_thrown = True
+    assert exception_thrown
+
+def test_create_light_string():
+    result = create_light_string("RRRR")
+    assert result = "RRRR\n"
+
+def test_hours_to_string():
+    result = hours_to_string(16)
+    assert result = "RRR0\nR000\n"
+
+def test_minutes_to_string():
+    result = minutes_to_string(56)
+    assert result = "YYYYYYYYYYY\nYOOO\n"
+
+def test_seconds_to_string_odd():
+    result = seconds_to_string(5)
+    assert result = 'O'
+
+def test_seconds_to_string_even():
+    result = seconds_to_string(8)
+    assert result = 'Y'
+
+def test_time_to_string()
+    irrelevant_year = 2000
+    irrelevant_day = 1
+    irrelevant_month = 1
+    result = time_to_string(datetime(irrelevant_year,irrelevant_day,irrelevant_month,12,56,1))
+    assert result = "O\nRROO\nRROO\nYYYYYYYYYYY\nYOOO\n"
+
+
